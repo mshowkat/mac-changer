c83c58277bd4128813e89f284ce6c10b6a0d21c5
mshowkat <bangleblaze@gmail.com>
2020-05-19 00:04:22 -0400

    init

---
+++ macChanger.py
@@ -0,0 +1,8 @@
+#!this is a mach changer program for changing your device mac adresses.
+
+import subprocess
+
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 08:00:27:1f:30:76", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig", shell=True)
